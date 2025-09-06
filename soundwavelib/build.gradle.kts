a72d7f9e4a555eccbd16a0aa3d758fbe654ea926
ultimateHandsomeBoy666 <jiujiuli@qq.com>
2025-09-07 03:09:53 +0800

    [feat] 添加发布配置

--- soundwavelib/build.gradle.kts
+++ soundwavelib/build.gradle.kts
@@ -1,7 +1,12 @@
 plugins {
     alias(libs.plugins.android.library)
     alias(libs.plugins.kotlin.android)
+    `maven-publish`
+    signing
 }
+
+group = "io.github.ultimatehandsomeboy666"
+version = "1.0.0"
 
 android {
     namespace = "com.soundwave.lib"
@@ -9,8 +14,6 @@ android {
 
     defaultConfig {
         minSdk = 21
-
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         consumerProguardFiles("consumer-rules.pro")
     }
 
@@ -29,6 +32,65 @@ android {
     }
     kotlinOptions {
         jvmTarget = "11"
+    }
+}
+
+afterEvaluate {
+    publishing {
+        publications {
+            create<MavenPublication>("release") {
+                from(components["release"])
+
+                pom {
+                    name.set("SoundWave")
+                    description.set("A SoundView that dances with volume changing")
+                    url.set("https://github.com/ultimateHandsomeBoy666/SoundView")
+
+                    licenses {
+                        license {
+                            name.set("The Apache License, Version 2.0")
+                            url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+                        }
+                    }
+                    developers {
+                        developer {
+                            id.set("ultimateHandsomeBoy666")
+                            name.set("bullfrog")
+                            email.set("jiujiuli@qq.com")
+                        }
+                    }
+                    scm {
+                        connection.set("scm:git:github.com/ultimateHandsomeBoy666/SoundWave.git")
+                        developerConnection.set("scm:git:ssh://github.com/ultimateHandsomeBoy666/SoundWave.git")
+                        url.set("https://github.com/ultimateHandsomeBoy666/SoundWave/tree/main")
+                    }
+                }
+            }
+        }
+        repositories {
+            maven {
+                name = "Sonatype"
+                val releasesRepoUrl = "https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/"
+                val snapshotsRepoUrl = "https://s01.oss.sonatype.org/content/repositories/snapshots/"
+                url = uri(if (version.toString().endsWith("SNAPSHOT")) snapshotsRepoUrl else releasesRepoUrl)
+                
+                credentials {
+                    username = project.properties["ossrhUsername"] as String?
+                    password = project.properties["ossrhPassword"] as String?
+                }
+            }
+        }
+    }
+}
+
+// 6. 配置 GPG 签名
+signing {
+    val signingKeyId = project.properties["signing.keyId"] as String?
+    val signingPassword = project.properties["signing.password"] as String?
+    val signingKey = project.properties["signing.secretKeyRingFile"] as String?
+    
+    if (signingKeyId != null) {
+        sign(publishing.publications)
     }
 }
 
